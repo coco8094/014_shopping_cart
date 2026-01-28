f0e50414697f4e135a4ae0bfa53277d8186797f2
coco <coco20130903@gmail.com>
2026-01-28 17:29:54 +0800

    更新了網頁標題的圖片

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    //vueDevTools(),
   ],
+  base: '/014_shopping_cart/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
